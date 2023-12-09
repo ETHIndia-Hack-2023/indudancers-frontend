4001665b0204724dcef4115300d58cc8ba6b5ac0
Ilya Klimenko <sn0wy-0wl@yandex.ru>
2023-12-09 08:45:28 +0300

    Improved avatar and profile pic

--- components/profile/profile.tsx
+++ components/profile/profile.tsx
@@ -2,7 +2,8 @@
 
 import { useAccount, useConnect, useDisconnect } from 'wagmi'
 import { InjectedConnector } from 'wagmi/connectors/injected'
-import { Button } from '../ui/button'
+import { Button } from '@/components/ui/button'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 export default function Profile() {
   const { address, isConnected } = useAccount()
@@ -13,8 +14,15 @@ export default function Profile() {
 
   if (isConnected)
     return (
-      <div className="flex flex-col w-full gap-4 p-4 bg-blue-900 rounded-md">
-        Connected to {address}
+      <div className="flex flex-row items-center w-full gap-4 rounded-md">
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <p className="text-ellipsis overflow-hidden whitespace-nowrap max-w-[200px]">
+          {' '}
+          {address}
+        </p>
         <Button onClick={() => disconnect()}>Disconnect</Button>
       </div>
     )
