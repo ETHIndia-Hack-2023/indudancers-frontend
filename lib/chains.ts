c2f36cbfeaebae89afcc5e30cabf38e3cc213643
asimaranov <a.simaranov@gmail.com>
2023-12-10 07:11:10 +0530

    Added networks

--- lib/chains.ts
+++ lib/chains.ts
@@ -26,9 +26,40 @@ export const stylusTestnet = defineChain({
     contracts: {},
   })
 
+  export const x1net = defineChain({
+    id: 195,
+    name: 'X1',
+    network: 'x1',
+    nativeCurrency: {
+      decimals: 18,
+      name: 'Ether',
+      symbol: 'ETH',
+    },
+    rpcUrls: {
+      default: {
+        http: ['https://testrpc.x1.tech'],
+        webSocket: ['wss://x1testws.okx.com'],
+      },
+      public: {
+        http: ['https://testrpc.x1.tech'],
+        webSocket: ['wss://x1testws.okx.com'],
+      },
+    },
+    blockExplorers: {
+      default: { name: 'Explorer', url: 'https://www.oklink.com/x1-test' },
+    },
+    contracts: {
+      multicall3: {
+        address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+        blockCreated: 5882,
+      },
+    },
+  })
+  
 export const SuppurtedChains = [
     stylusTestnet,
     scrollSepolia,
     celoAlfajores,
-    mantleTestnet
+    mantleTestnet,
+    x1net
 ]
